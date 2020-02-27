65c019350e50d45d7e19ba8141b25edf6f579fa2
Gabriel Cabrera <gabriel.sigurd.cabrera@gmail.com>
2020-02-27 17:01:42 +0100

    YEET

--- Project 1/frontend/project_1b.cpp
+++ Project 1/frontend/project_1b.cpp
@@ -26,9 +26,7 @@ double monte_carlo_1b(double step_size, int steps, int cycles, int N,
   // Loading Percentage
   double perc = 0;
   // Loading New Percentage
-  double New_perc = 0;
-  // Loading Bar Output
-  double perc_disp = 0;
+  double perc_new;
   // Old Probability
   double Psi_old;
   // Old Energy
@@ -43,6 +41,20 @@ double monte_carlo_1b(double step_size, int steps, int cycles, int N,
     Psi_old = PDF(P);
     // Old Energy
     E_old = PDF.energy(P);
+    // Equilibriation
+    for (int j = 0; j < equi_steps; j++) {
+
+      P_new = random_walk(PDF, P, step_size);
+      Psi_new = PDF(P_new);
+
+      W = std::pow(std::abs(Psi_new/Psi_old), 2);
+      r = rand_double(0, 1);
+
+      if (W > r) {
+        P = P_new;
+      }
+    }
+
     // Samples per Cycle
     for (int j = 0; j < steps; j++) {
 
@@ -53,16 +65,27 @@ double monte_carlo_1b(double step_size, int steps, int cycles, int N,
       r = rand_double(0, 1);
       E_new = PDF.energy(P_new);
 
-      if ((j < equi_steps && W > r) || (j >= equi_steps && (W > r))) {
+      if (W > r) {
         P = P_new;
         E_old = E_new;
         Psi_old = Psi_new;
       }
     }
+
     E_tot += E_old;
     accum_cycles += 1;
-    // std::cout << "\r" << ""
-    std::cout << E_tot/accum_cycles << " " << std::endl;
+    perc_new = std::trunc(100*i/cycles);
+    if (perc_new > perc) {
+      perc = perc_new;
+      if (perc < 10) {
+        std::cout << "\r   " << perc << "%" << std::flush;
+      } else if (10 <= perc && perc < 100) {
+        std::cout << "\r  " << perc << "%" << std::flush;
+      } else {
+        std::cout << "\r " << perc << "%" << std::flush;
+      }
+    }
+    // std::cout << E_tot/accum_cycles << " " << std::endl;
   }
   return E_tot/accum_cycles;
 }
@@ -70,8 +93,8 @@ double monte_carlo_1b(double step_size, int steps, int cycles, int N,
 void run_1b() {
 
   double step_size = 1E-5;    // Step size during random walk
-  int steps = 1E5;            // Number of Monte-Carle steps per cycle
-  int cycles = 1E4;           // Number of Monte-Carlo cycles
+  int steps = 1E4;            // Number of Monte-Carle steps per cycle
+  int cycles = 1E3;           // Number of Monte-Carlo cycles
   int N = 1;                  // Number of Particles
   int x_max = 1;              // Maximum Initial Distance From Origin
   double a = 1E-8;            // Atomic Radius
@@ -79,8 +102,6 @@ void run_1b() {
   double omega_z = 1;         // Harmonic Oscillator Z-Frequency
   int equi_steps = 1E3;       // Number of Steps Dedicated to Equilibriation
 
-  double* alphas = new double[5] {6E-1, 5E-1, 4E-1, 3E-1};  // Hyperparameter
-  double* betas = new double[5] {6E-1, 5E-1, 4E-1, 3E-1};   // Hyperparameter
   double* energies = new double[5];                         // Final Energies
 
   double alpha; double beta;
