522a23e9a0652ece09236658d54ba6ef450dca9c
gabrielscabrera <gabriel.sigurd.cabrera@gmail.com>
2020-03-02 17:37:21 +0100

    YEET

--- Project 1/frontend/project_1b.cpp
+++ Project 1/frontend/project_1b.cpp
@@ -4,33 +4,31 @@
 #include "../backend/Psi.h"
 #include "../backend/random.h"
 
-double monte_carlo_1b(double step_size, int steps, int cycles, int N,
-                      double x_max, double alpha, double beta, double a,
-                      double omega, double omega_z, int equi_steps) {
+double* monte_carlo_1b(double step_size, int steps, int cycles, int N,
+                       double x_max, double alpha, double beta, double a,
+                       double omega, double omega_z, int equi_steps) {
   // Initialize Wave Function
   Psi PDF(alpha, beta, a, omega, omega_z);
   // Initialize Secondary Random Particle Array
   Mat P_new(N,3);
   // New Probability
-  double Psi_new;
+  double Psi_new = 0;
   // New Energy
   double E_new;
   // Total Energy
   double E_tot = 0;
+  // Total Squared Energy
+  double E_squared = 0;
   // Averaging Factor
   int accum_cycles = 0;
   // Acceptance Ratio
   double W;
-  // Random Number between 0 and 1
-  double r;
   // Loading Percentage
   double perc = 0;
   // Loading New Percentage
   double perc_new;
   // Old Probability
-  double Psi_old;
-  // Old Energy
-  double E_old;
+  double Psi_old = 0;
   // Initialize Random Particle Array
   Mat P = random_particles(N, -x_max, x_max);
   // Monte-Carlo Cycles
@@ -39,42 +37,43 @@ double monte_carlo_1b(double step_size, int steps, int cycles, int N,
     Mat P = random_particles(N, -x_max, x_max);
     // Old Probability
     Psi_old = PDF(P);
-    // Old Energy
-    E_old = PDF.energy(P);
     // Equilibriation
     for (int j = 0; j < equi_steps; j++) {
-
+      // Generate New Movement
       P_new = random_walk(PDF, P, step_size);
+      // Get New Probability
       Psi_new = PDF(P_new);
-
+      // Calculate Ratio of Probabilities
       W = Psi_new/Psi_old;
       W *= W;
-      r = rand_double(0, 1);
-
-      if (W > r) {
+      // Determine whether or not to accept movement
+      if (W > rand_double(0, 1)) {
         P = P_new;
       }
     }
 
     // Samples per Cycle
     for (int j = 0; j < steps; j++) {
-
+      // Generate New Movement
       P_new = random_walk(PDF, P, step_size);
+      // Get New Probability
       Psi_new = PDF(P_new);
-
+      // Calculate Ratio of Probabilities
       W = Psi_new/Psi_old;
       W *= W;
-      r = rand_double(0, 1);
-      E_new = PDF.energy(P_new);
 
-      if (W > r) {
+      // Determine whether or not to accept movement
+      if (W > rand_double(0, 1)) {
         P = P_new;
-        E_old = E_new;
         Psi_old = Psi_new;
       }
     }
 
-    E_tot += E_old;
+    // Set New Energy
+    E_new = PDF.energy(P_new);
+
+    E_tot += E_new;
+    E_squared += E_new*E_new;
     accum_cycles += 1;
     perc_new = std::trunc(100*i/cycles);
     if (perc_new > perc) {
@@ -87,9 +86,15 @@ double monte_carlo_1b(double step_size, int steps, int cycles, int N,
         std::cout << "\r " << perc << "%" << std::flush;
       }
     }
-    // std::cout << E_tot/accum_cycles << " " << std::endl;
   }
-  return E_tot/accum_cycles;
+
+  // Preparing Function Output
+  double* output = new double[3];
+  output[0] = E_tot/accum_cycles;
+  output[1] = E_squared/accum_cycles;
+  output[2] = Psi_old;
+
+  return output;
 }
 
 void run_1b() {
@@ -104,16 +109,21 @@ void run_1b() {
   double omega_z = 1;         // Harmonic Oscillator Z-Frequency
   int equi_steps = 1E3;       // Number of Steps Dedicated to Equilibriation
 
-  double* energies = new double[5];                         // Final Energies
-
+  // Final Energies
+  double* energies = new double[5];
+  double* energies_squared = new double[5];
+  double* probabilities = new double[5];
   double alpha; double beta;
 
   for (int i = 0; i < 5; i++) {
     alpha = 0.2;
-    // alpha = alphas[i];
     beta = 1.5;
-    // beta = betas[i];
-    energies[i] = monte_carlo_1b(step_size, steps, cycles, N, x_max, alpha,
-                                 beta, a, omega, omega_z, equi_steps);
+
+    // Function Outputs
+    double* output = new double [3];
+    output = monte_carlo_1b(step_size, steps, cycles, N, x_max, alpha,
+                            beta, a, omega, omega_z, equi_steps);
+    delete[] output;
   }
+  delete[] energies;
 }
--- Project 1/frontend/project_1b.h
+++ Project 1/frontend/project_1b.h
@@ -1,9 +1,9 @@
 #ifndef PROJECT_1B_H
 #define PROJECT_1B_H
 
-void monte_carlo_1b(double step_size, int steps, int cycles, int N,
-                    double x_max, double alpha, double beta, double a,
-                    double omega, double omega_z, int equi_steps);
+double* monte_carlo_1b(double step_size, int steps, int cycles, int N,
+                       double x_max, double alpha, double beta, double a,
+                       double omega, double omega_z, int equi_steps);
 
 void run_1b();
 
